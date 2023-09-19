1abbbd54d414b49e2121bd6e8754a61be0351e37
Yohei Okamura <120467663+yohei03@users.noreply.github.com>
2023-09-20 00:16:07 +0900

    httpサーバーの実装場所の変更

--- src/main.py
+++ src/main.py
@@ -1,10 +1,27 @@
 import discord
 from dotenv import load_dotenv
 import os
-from time import time
 import sendRequest 
+from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
+import threading
+import datetime
+import time
 
 load_dotenv()
+
+ADDRESS = "127.0.0.1"
+PORT =5000
+
+class MyHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        message = "Hello, world!"
+        self.wfile.write(bytes(message, "utf8"))
+        return
+
+httpd = ThreadingHTTPServer((ADDRESS, PORT), MyHandler)
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -18,7 +35,7 @@ async def on_voice_state_update(member, before, after):
   #入室の場合
   if (before.channel is None) and (after.channel is not None):
     entranceInfo = {
-      "timestamp": time(),
+      "timestamp": time.time(),
       "event": "entrance",
       "userId": str(member.id),
       "userName": member.name,
@@ -43,4 +60,16 @@ async def on_voice_state_update(member, before, after):
 
   return
 
-client.run(token)
+
+def loop():
+    while(True):
+      print("called")
+      time.sleep(10)
+
+
+
+
+if __name__ == "__main__":
+   thread_1 = threading.Thread(target=httpd.serve_forever)
+   thread_1.start()
+   client.run(token)
